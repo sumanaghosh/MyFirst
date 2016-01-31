b45977b57d1944b09a5c38aadbe9264b6da5bf00
Shashi <shashikamal0601.slg@gmail.com>
2016-01-31 11:57:23 +0530

    My first commit

---
+++ MyFirstWebPgChk.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function check()
+{
+	var c=document.forms ["f1"]["fn"].value;
+	if (c==null || c=="")
+		alert ("Please fill out your Name!");
+	document.forms ["f1"]["fn"].focus;
+	return false;
+	
+	var c=document.forms
+}
